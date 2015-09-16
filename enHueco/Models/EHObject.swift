5f0433f2d2f34a864bdc0077d307d3b551e629cc
Diego Montoya <diegomontoyas@gmail.com>
2015-09-16 15:15:49 -0500

    Implemented EHObject. Minor interface changes. Code Cleaning.

---
+++ enHueco/Models/EHObject.swift
@@ -0,0 +1,13 @@
+//
+//  EHObject.swift
+//  enHueco
+//
+//  Created by Diego on 9/16/15.
+//  Copyright © 2015 Diego Gómez. All rights reserved.
+//
+
+import Cocoa
+
+class EHObject: NSObject {
+
+}
