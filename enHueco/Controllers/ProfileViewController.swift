03e59b15a23d6ad8c42c2b1ebc2113dcb6a5eac1
Diego Montoya Sefair <diegomontoyas@gmail.com>
2015-10-21 02:44:12 -0500

    Another minor UI fix.

--- enHueco/Controllers/ProfileViewController.swift
+++ enHueco/Controllers/ProfileViewController.swift
@@ -28,41 +28,7 @@ class ProfileViewController: UIViewController
         usernameLabel.text = system.appUser.username
         
         backgroundImageView.alpha = 0
-        imageImageView.alpha = 1
-        
-        if let imageURL = system.appUser.imageURL
-        {
-            imageImageView.hidden = true
-            backgroundImageView.hidden = true
-           
-            imageImageView.sd_setImageWithURL(imageURL, completed: { (_, error, _, _) -> Void in
-                
-                if error == nil
-                {
-                    UIView.animateWithDuration(0.4)
-                    {
-                        self.imageImageView.alpha = 1
-                    }
-                }
-            })
-            
-            backgroundImageView.sd_setImageWithURL(imageURL, completed: { (_, error, _, _) -> Void in
-                
-                if error == nil
-                {
-                    UIView.animateWithDuration(0.4)
-                    {
-                        self.backgroundImageView.image = self.backgroundImageView.image?.applyBlurWithRadius(40, tintColor: UIColor(white: 0.2, alpha: 0.5), saturationDeltaFactor: 1.8, maskImage: nil)
-                        self.backgroundImageView.alpha = 1
-                    }
-                }
-            })
-        }
-        else
-        {
-            imageImageView.hidden = true
-            backgroundImageView.hidden = true
-        }
+        imageImageView.alpha = 0
         
         imageImageView.contentMode = .ScaleAspectFill
         backgroundImageView.contentMode = .ScaleAspectFill
@@ -92,6 +58,40 @@ class ProfileViewController: UIViewController
     override func viewDidAppear(animated: Bool)
     {
         super.viewDidAppear(animated)
+        
+        if let imageURL = system.appUser.imageURL
+        {
+            imageImageView.hidden = false
+            backgroundImageView.hidden = false
+            
+            imageImageView.sd_setImageWithURL(imageURL, completed: { (_, error, _, _) -> Void in
+                
+                if error == nil
+                {
+                    UIView.animateWithDuration(0.4)
+                    {
+                        self.imageImageView.alpha = 1
+                    }
+                }
+            })
+            
+            backgroundImageView.sd_setImageWithURL(imageURL, completed: { (_, error, _, _) -> Void in
+                
+                if error == nil
+                {
+                    UIView.animateWithDuration(0.4)
+                    {
+                        self.backgroundImageView.image = self.backgroundImageView.image?.applyBlurWithRadius(40, tintColor: UIColor(white: 0.2, alpha: 0.5), saturationDeltaFactor: 1.8, maskImage: nil)
+                        self.backgroundImageView.alpha = 1
+                    }
+                }
+            })
+        }
+        else
+        {
+            imageImageView.hidden = true
+            backgroundImageView.hidden = true
+        }
         
         system.appUser.fetchUpdatesForAppUserAndSchedule()
     }
