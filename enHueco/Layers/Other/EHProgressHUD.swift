5c472958d78c455d159c27bb39a539c6bd09135c
Diego Montoya Sefair <diegomontoyas@gmail.com>
2016-03-09 21:24:35 -0500

    Add new convenient wrappers: EHNotifications to show in app banners and EHProgressHUD to show progress indicators.

---
+++ enHueco/Layers/Other/EHProgressHUD.swift
@@ -0,0 +1,28 @@
+//
+//  EHProgressHUD.swift
+//  enHueco
+//
+//  Created by Diego Montoya Sefair on 3/8/16.
+//  Copyright © 2016 Diego Gómez. All rights reserved.
+//
+
+import UIKit
+import MRProgress
+
+class EHProgressHUD: NSObject
+{
+    class func showSpinnerInView(view: UIView, title: String? = nil, animated: Bool = true)
+    {
+        MRProgressOverlayView.showOverlayAddedTo(view, title: title ?? "", mode: MRProgressOverlayViewMode.Indeterminate, animated: animated).setTintColor(EHInterfaceColor.mainInterfaceColor)
+    }
+    
+    class func dismissSpinnerForView(view: UIView, animated: Bool = true)
+    {
+        MRProgressOverlayView.dismissOverlayForView(view, animated: animated)
+    }
+    
+    class func dismissAllSpinnersForView(view: UIView, animated: Bool = true)
+    {
+        MRProgressOverlayView.dismissAllOverlaysForView(view, animated: animated)
+    }
+}
